d4749be90fc847f110bc089a0d31ed4c53a80275
goldenglorys <olusolagloryolamide@gmail.com>
2021-04-21 13:24:30 +0100

    Fix swagger documentation respone error that is returning html chars instead of json

--- dist/middleware/apiDocs.js
+++ dist/middleware/apiDocs.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.handleAPIDocs = void 0;
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const swagger_json_1 = __importDefault(require("../config/swagger.json"));
-const handleAPIDocs = (router) => router.use("/api", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
+const handleAPIDocs = (router) => router.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
 exports.handleAPIDocs = handleAPIDocs;
 //# sourceMappingURL=apiDocs.js.map
